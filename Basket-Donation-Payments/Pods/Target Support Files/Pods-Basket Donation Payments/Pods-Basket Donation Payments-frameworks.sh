ff338e01e7003c3f297d5cae366b576dfe5e26e4
Kristie Huang <kristiehuang@fb.com>
2020-07-13 16:07:01 -0700

    Add Braintree pods

--- Basket-Donation-Payments/Pods/Target Support Files/Pods-Basket Donation Payments/Pods-Basket Donation Payments-frameworks.sh
+++ Basket-Donation-Payments/Pods/Target Support Files/Pods-Basket Donation Payments/Pods-Basket Donation Payments-frameworks.sh
@@ -198,10 +198,12 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Braintree/Braintree.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Braintree/Braintree.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
