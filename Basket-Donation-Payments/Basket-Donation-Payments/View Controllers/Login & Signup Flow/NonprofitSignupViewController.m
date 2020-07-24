32d7728668a1eb0829ff14b1b70699115e2f9894
Kristie Huang <kristiehuang@fb.com>
2020-07-24 09:50:05 -0700

    Unwind segue in creation flow passes data back and forth

--- Basket-Donation-Payments/Basket-Donation-Payments/View Controllers/Login & Signup Flow/NonprofitSignupViewController.m
+++ Basket-Donation-Payments/Basket-Donation-Payments/View Controllers/Login & Signup Flow/NonprofitSignupViewController.m
@@ -13,7 +13,6 @@
 #import <UIKit/UIKit.h>
 
 @interface NonprofitSignupViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate>
-@property (nonatomic, strong) Nonprofit *nonprofit;
 @property (weak, nonatomic) IBOutlet UIImageView *nonprofitProfileImageView;
 @property (weak, nonatomic) IBOutlet UITextField *nonprofitNameTextField;
 @property (weak, nonatomic) IBOutlet UITextView *nonprofitDescriptionTextView;
@@ -21,6 +20,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *nonprofitWebsiteTextField;
 //TODO: set up with Paypal, Braintree?
 @property (nonatomic) BOOL finishedSavingBoth;
+
+
 
 @end
 
@@ -30,19 +31,29 @@
     [super viewDidLoad];
     self.nonprofitDescriptionTextView.delegate = self;
     self.finishedSavingBoth = NO;
-    self.nonprofit = [Nonprofit new];
-    self.nonprofitDescriptionTextView.text = @"Describe your nonprofit in 100 words or less.";
-    self.nonprofitDescriptionTextView.textColor = [UIColor lightGrayColor];
-    //TODO: twitter-like word-limitations
     
+    if (self.nonprofit) {
+        self.nonprofitProfileImageView.image = [Utils getImageFromPFFile:self.nonprofit.profilePicFile];
+        self.nonprofitNameTextField.text = self.nonprofit.nonprofitName;
+        self.nonprofitDescriptionTextView.text = self.nonprofit.nonprofitDescription;
+        self.nonprofitCategoryTextField.text = self.nonprofit.category;
+        self.nonprofitWebsiteTextField.text = self.nonprofit.websiteUrlString;
+    } else {
+        self.nonprofit = [Nonprofit new];
+        self.nonprofitDescriptionTextView.text = @"Describe your nonprofit in 100 words or less.";
+        self.nonprofitDescriptionTextView.textColor = [UIColor lightGrayColor];
+        //TODO: twitter-like word-limitations
+        //TODO: figure out document uplaod
+        // https://stackoverflow.com/questions/37296929/implement-document-picker-in-swift-ios
+    }
 
-    //TODO: figure out document uplaod
-    // https://stackoverflow.com/questions/37296929/implement-document-picker-in-swift-ios
 }
 
 - (IBAction)addPictureButtonTapped:(id)sender {
     [Utils createImagePickerVCWithVC:self];
 }
+
+
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
     UIImage *editedImage = info[UIImagePickerControllerEditedImage];
@@ -55,16 +66,10 @@
 }
 
 -(void)saveNonprofitAndUserToParse {
-    self.nonprofit.profilePicFile = [Utils getFileFromImage:self.nonprofitProfileImageView.image];
-    self.nonprofit.nonprofitName = self.nonprofitNameTextField.text;
-    self.nonprofit.nonprofitDescription = self.nonprofitDescriptionTextView.text;
-    self.nonprofit.category = self.nonprofitCategoryTextField.text;
-    self.nonprofit.websiteUrlString = self.nonprofitWebsiteTextField.text;
-    self.user.nonprofit = self.nonprofit;
+    [self saveNonprofitDataToNonprofitObject];
     
     NSOperationQueue *opQueue = [NSOperationQueue new];
 
-    
     NSBlockOperation *signupUserOp = [NSBlockOperation new];
     signupUserOp.queuePriority = NSOperationQueuePriorityHigh;
     __weak NSBlockOperation *weakSignupUserOp = signupUserOp;
@@ -117,20 +122,26 @@
     }
     [opQueue addOperation:loginSegueOp];
     
-    
-    
 
 
 }
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)saveNonprofitDataToNonprofitObject {
+    self.nonprofit.profilePicFile = [Utils getFileFromImage:self.nonprofitProfileImageView.image];
+    self.nonprofit.nonprofitName = self.nonprofitNameTextField.text;
+    self.nonprofit.nonprofitDescription = self.nonprofitDescriptionTextView.text;
+    self.nonprofit.category = self.nonprofitCategoryTextField.text;
+    self.nonprofit.websiteUrlString = self.nonprofitWebsiteTextField.text;
+    self.user.nonprofit = self.nonprofit;
+}
+
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"unwindToUserSignup"]) {
+    } else {
+        [self saveNonprofitDataToNonprofitObject];
+    }
 }
-*/
 
 - (void)textViewDidBeginEditing:(UITextView *)textView {
     if ([textView.textColor isEqual:[UIColor lightGrayColor]]) {
