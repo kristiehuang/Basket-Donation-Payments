99fbecfa03719a92135404dd4fb0237d90c3e9e8
Kristie Huang <kristiehuang@fb.com>
2020-07-29 16:50:11 -0700

    Add server endpoint to create new connected account

--- Basket-Donation-Payments/server.js
+++ Basket-Donation-Payments/server.js
@@ -36,6 +36,20 @@ app.post("/create-new-customer", async (req, res) => {
     });
 });
 
+app.post("/create-new-connected-account", async (req, res) => {
+    const { authorizationCode, email } = req.body;
+    // Create a new Stripe Customer with the order amount and currency
+    const response = await stripe.oauth.token({
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        email: email
+    });
+
+    res.send({
+        connectedAccountId: response.stripe_user_id
+    });
+});
+
 
 const calculateTransferAmount = (totalAmount, basketItems) => {
     // Replace this constant with a calculation of the order's amount
