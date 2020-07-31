bce290a665ad8a769b4121d002e6499e9ae8d3a6
Kristie Huang <kristiehuang@fb.com>
2020-07-30 17:09:22 -0700

    Add tap to dismiss keyboard on loginScreen

--- Basket-Donation-Payments/Basket-Donation-Payments/View Controllers/Login & Signup Flow/InitialLoginViewController.m
+++ Basket-Donation-Payments/Basket-Donation-Payments/View Controllers/Login & Signup Flow/InitialLoginViewController.m
@@ -11,7 +11,7 @@
 #import <Parse/Parse.h>
 #import "Utils.h"
 
-@interface InitialLoginViewController ()
+@interface InitialLoginViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
@@ -21,13 +21,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.usernameTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self.view action:@selector(endEditing:)];
+    [self.view addGestureRecognizer:tap];
 }
 
 - (IBAction)loginButtonTapped:(id)sender {
-    [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+    NSString *inputUsername = self.usernameTextField.text;
+    NSString *inputPassword = self.passwordTextField.text;
+    if ((inputUsername.length == 0) || (inputPassword.length == 0)) {
+        UIAlertController *alert = [Utils createAlertControllerWithTitle:@"Empty username or password." andMessage:@"Please input a username and password." okCompletion:nil cancelCompletion:nil];
 
+        [self presentViewController:alert animated:YES completion:nil];
+    } else {
+        [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+    }
 }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    if ([textField isEqual:self.passwordTextField]) {
+        [self loginButtonTapped:nil];
+    }
+    return YES;
+}
+
 
 - (IBAction)signupButtonTapped:(id)sender {
     [self performSegueWithIdentifier:@"signupSegue" sender:nil];
@@ -44,13 +63,6 @@
         userSignupVC.password = inputPassword;
         
     } else if ([segue.identifier isEqualToString:@"loginSegue"]) {
-        
-        if ((inputUsername.length == 0) || (inputPassword.length == 0)) {
-            UIAlertController *alert = [Utils createAlertControllerWithTitle:@"Empty username or password." andMessage:@"Please input a username and password." okCompletion:nil cancelCompletion:nil];
-
-            [self presentViewController:alert animated:YES completion:nil];
-        }
-        
         [PFUser logInWithUsernameInBackground:inputUsername password:inputPassword block:^(PFUser * _Nullable user, NSError * _Nullable error) {
             if (error != nil) {
                 UIAlertController *alert = [Utils createAlertControllerWithTitle:@"Oops, couldn't log you in." andMessage:error.localizedDescription okCompletion:nil cancelCompletion:nil];
